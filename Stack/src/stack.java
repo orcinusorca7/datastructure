d790b1750d8c18716c3bfbadc89025420ad195a8
ChandimaHerath <chandimaherath@hotmail.com>
2021-05-06 18:44:43 +0530

    pop and push methods implemented

--- Stack/src/stack.java
+++ Stack/src/stack.java
@@ -28,13 +28,38 @@ public class stack
          System.out.println("\b\b]");
      }
 
-     public void push(){
+     public void push(int number){
 
+         if (numbers == null){
+            numbers = new int[1];
+            numbers[0] = number;
+        }
+        else{
+            int[] temp = new int[numbers.length + 1];
+            for (int i=0; i< numbers.length; i++){
+                temp[i] = numbers[i];
+            }
+            temp[temp.length -1] = number;
+            numbers = temp;
+        }
      }
 
      public void pop(){
-
+        if (numbers == null)
+        { System.err.println("empty stack");
+            return;
+        }else if (numbers.length==1){
+            numbers= null;
+            return;
+        }
+        int[] temp = new int [numbers.length-1];
+        for (int i=0; i< temp.length; i++)
+        {
+            temp[i] = numbers[i];
+        }
+        numbers = temp;
      }
+
 
      public int size(){
         return numbers.length;
