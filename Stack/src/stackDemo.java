ebf3924c7557dd3d0051007fef01428f4016df90
ChandimaHerath <chandimaherath@hotmail.com>
2021-05-06 20:13:32 +0530

    implemented main class

--- Stack/src/stackDemo.java
+++ Stack/src/stackDemo.java
@@ -3,6 +3,18 @@ public class stackDemo {
 
     public static void main(String[] args) {
         stack Stack = new stack();
+        System.out.println(Stack.empty());
+        Stack.push(10);
+        Stack.push(20);
+        Stack.push(30);
+        System.out.println(Stack.size());
+        Stack.pop();
+        Stack.peek();
+        System.out.println(Stack.contains(40));
+        Stack.clear();
+        Stack.print();
+
+
 
     }
 }
