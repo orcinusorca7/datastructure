bd8149e5ee4f986fac6f266150a2e28a175a57d1
ChandimaHerath <chandimaherath@hotmail.com>
2021-05-06 11:14:04 +0530

    pop method implemented

--- Stack/src/stack.java
+++ Stack/src/stack.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class stack {
+import jdk.nashorn.internal.IntDeque;
+
+import java.util.Stack;
+
+public class stack
+{
+
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack();
+
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+
+        int number = stack.pop();
+        System.out.println("Removed Element: " + number);
+
+        System.out.println("Stack: " + stack);
+
+    }
+
+
 }
+
