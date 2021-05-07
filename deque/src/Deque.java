0e4de7db7221594c0a6d876886b91bb10e7e3895
ChandimaHerath <chandimaherath@hotmail.com>
2021-05-07 10:33:33 +0530

    dequeue front method roughly done. not tested

--- deque/src/Deque.java
+++ deque/src/Deque.java
@@ -4,14 +4,25 @@ class Deque extends Queue {
 
     private int[] numbers;
 
-    public void enqueueRear(int number){
+    public void enqueueRear(int number){  if (numbers == null){
+        numbers = new int[1];
+        numbers[0] = number;
+    }
+    else{
+        int[] temp = new int[numbers.length + 1];
+        for (int i=0; i< numbers.length; i++){
+            temp[i] = numbers[i+1];
+        }
+        temp[temp.length -1] = number;
+        numbers = temp;
+    }
 
     }
 
 
     public void dequeueFront(){
         if (numbers == null)
-        { System.err.println("empty stack");
+        { System.err.println("empty deque");
             return;
         }else if (numbers.length==1){
             numbers= null;
@@ -26,55 +37,8 @@ class Deque extends Queue {
 
     }
 
-  //  private int[] numbers;
 
-//    public boolean empty(){
-//
-//        return numbers == null;
-//    }
-//
-//    public int size(){
-//        return numbers.length;
-//    }
-//
-//
-//
-//    public int peek (){
-//        if (empty())
-//        {
-//            System.out.println("queue is empty");
-//
-//        }
-//
-//        return numbers[numbers.length-1];
-//    }
-//
-//
-//    public boolean contains(int number){
-//        if(empty()){
-//            return false;
-//        }
-//        for(int i = 0; i<numbers.length; i++){
-//            if(numbers[i] == number){
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//
-//
-//    public void print(){
-//        if (empty()){
-//            System.out.println("[]");
-//            return;
-//        }
-//
-//        System.out.println("[");
-//        for(int i= numbers.length-1; i>=0 ; i--){
-//            System.out.println(numbers[i] + ",");
-//        }
-//        System.out.println("\b\b]");
-//    }
+
 
 
 
